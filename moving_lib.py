aa4bd43b1fe99dedad405acfedf7e5f963dc6b80
Amy Rieck <amyrieckhome@gmail.com>
2018-04-19 17:35:11 +1000

    will

---
+++ moving_lib.py
@@ -0,0 +1,16 @@
+"""****************************************************************************
+* mvoing_lib.py
+* Alistair Moffat Appreciation Society
+* Amy Rieck and Luke Hedt
+* lhedt@student.unimelb.edu.au
+*
+* Date: 2018/04/19
+*
+* Comments: It Begins
+*****************************************************************************"""
+""" For moving """
+def moving_phase(self, turns):
+
+
+
+	return new_pos, old_pos
---
+++ placing_lib.py
@@ -0,0 +1,17 @@
+"""****************************************************************************
+* placing_lib.py
+* Alistair Moffat Appreciation Society
+* Amy Rieck and Luke Hedt
+* lhedt@student.unimelb.edu.au
+*
+* Date: 2018/04/19
+*
+* Comments: It Begins
+*****************************************************************************"""
+""" For placing """
+def placing_phase(self, turns):
+
+
+	return move
+
+
