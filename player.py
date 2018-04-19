88d5aaaf13b9f7fa3b1e231f97b78817438948b7
PhalanxHead <lhedt@hotmail.com>
2018-04-19 15:44:03 +1000

    Init Commit 1.0

---
+++ player.py
@@ -0,0 +1,10 @@
+###############################################################################
+# player.py
+# Alistair Moffat Appreciation Society
+# Amy Rieck and Luke Hedt
+# lhedt@student.unimelb.edu.au
+#
+# Date: 2018/04/19
+#
+# Comments: It Begins
+###############################################################################
