fa654f6f205cd6e663c286019a35b237de98b6a1
Amy Rieck <amyrieckhome@gmail.com>
2018-04-19 17:08:08 +1000

    d

--- player.py
+++ player.py
@@ -8,3 +8,14 @@
 #
 # Comments: It Begins
 ###############################################################################
+
+class Player:
+
+	def __init__(self, player_class, colour):
+
+
+    def update(self, move):
+        #
+    def action(self, turns):
+        
+
