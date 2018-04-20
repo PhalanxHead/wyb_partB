cef32f03635f20d68a59d114729b43a51d88fc0e
Amy Rieck <amyrieckhome@gmail.com>
2018-04-20 21:27:46 +1000

    stuff

--- moving_lib.py
+++ moving_lib.py
@@ -21,39 +21,50 @@ def moving_phase(self, turns):
 # Very Basic Untailored Minimax Implementation
 # From http://giocc.com/concise-implementation-of-minimax-through-higher-order-functions.html
 def minimax(game_state):
-  moves = game_state.get_available_moves()
-  best_move = moves[0]
-  best_score = float('-inf')
-  for move in moves:
-    clone = game_state.next_state(move)
-    score = min_play(clone)
-    if score > best_score:
-      best_move = move
-      best_score = score
+	moves = game_state.get_available_moves()
+  	best_move = moves[0]
+  	best_score = float('-inf')
+  	
+  	for move in moves:
+		clone = game_state.next_state(move)
+    	score = min_play(clone)
+    	
+    	if score > best_score:
+      		best_move = move
+      		best_score = score
+  
   return best_move
 
 def min_play(game_state):
-  if game_state.is_gameover():
-    return evaluate(game_state)
-  moves = game_state.get_available_moves()
-  best_score = float('inf')
-  for move in moves:
-    clone = game_state.next_state(move)
-    score = max_play(clone)
-    if score < best_score:
-      best_move = move
-      best_score = score
+  	if game_state.is_gameover():
+    	return evaluate(game_state)
+  
+	moves = game_state.get_available_moves()
+  	best_score = float('inf')
+  	
+  	for move in moves:
+    	clone = game_state.next_state(move)
+    	score = max_play(clone)
+    
+    	if score < best_score:
+      		best_move = move
+      	best_score = score
+  
   return best_score
 
 def max_play(game_state):
-  if game_state.is_gameover():
-    return evaluate(game_state)
+  	if game_state.is_gameover():
+    	return evaluate(game_state)
+  
   moves = game_state.get_available_moves()
   best_score = float('-inf')
-  for move in moves:
-    clone = game_state.next_state(move)
-    score = min_play(clone)
+  	
+  	for move in moves:
+    	clone = game_state.next_state(move)
+    	score = min_play(clone)
+    
     if score > best_score:
       best_move = move
       best_score = score
+  
   return best_score
